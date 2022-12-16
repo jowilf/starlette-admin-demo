83e9e9dd04c5d702c1963592822b5befa2bf1311
jowilf <hounonj@gmail.com>
2022-12-17 00:40:03 +0100

    Bump starlette-admin version to 0.5.0

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 from starlette.applications import Starlette
-from starlette.middleware import Middleware
-from starlette.middleware.sessions import SessionMiddleware
 from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-from app.config import config
 from app.mongoengine import admin as admin_mongo
 from app.odmantic import admin as admin_odm
 from app.sqla import admin as admin_sqla
@@ -21,8 +18,7 @@ app = Starlette(
     routes=[
         Route("/", homepage),
         Mount("/statics", app=StaticFiles(directory="statics"), name="statics"),
-    ],
-    middleware=[Middleware(SessionMiddleware, secret_key=config.secret)],
+    ]
 )
 admin_sqla.mount_to(app)
 admin_mongo.mount_to(app)
