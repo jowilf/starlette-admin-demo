d14d58d1f8627c7122805e2dfa70179eb11e033d
jowilf <hounonj@gmail.com>
2023-02-02 05:22:20 -0600

    odmantic: disable Author deletion

--- app/odmantic/views.py
+++ app/odmantic/views.py
@@ -9,6 +9,9 @@ from app.odmantic.models import Author
 class AuthorView(ModelView):
     exclude_fields_from_list = [Author.addresses]
 
+    def can_delete(self, request: Request) -> bool:
+        return False
+
     async def repr(self, obj: Any, request: Request) -> str:
         assert isinstance(obj, Author)
         return obj.first_name + " " + obj.last_name
