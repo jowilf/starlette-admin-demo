f0c3e44934b95caf701763a8ef04df343a2c7ebe
Jocelin Hounon <hounonj@gmail.com>
2026-07-12 15:00:02 -0700

    Enable mysql_native_password authentication and update MySQL image version in docker-compose.yml

--- db-init/umami-init.sh
+++ db-init/umami-init.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS umami;
-    CREATE USER IF NOT EXISTS 'umami'@'%' IDENTIFIED BY '${UMAMI_DB_PASSWORD}';
+    CREATE USER IF NOT EXISTS 'umami'@'%' IDENTIFIED WITH mysql_native_password BY '${UMAMI_DB_PASSWORD}';
     GRANT ALL PRIVILEGES ON umami.* TO 'umami'@'%';
     FLUSH PRIVILEGES;
 EOSQL
