d3426ad85bf5c5c3b772c71d6d2132ca5aa08ff1
Jocelin Hounon <hounonj@gmail.com>
2026-07-12 15:24:59 -0700

    Remove umami user creation script and update MySQL configuration in docker-compose.yml

--- db-init/umami-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS umami;
-    CREATE USER IF NOT EXISTS 'umami'@'%' IDENTIFIED WITH mysql_native_password BY '${UMAMI_DB_PASSWORD}';
-    GRANT ALL PRIVILEGES ON umami.* TO 'umami'@'%';
-    FLUSH PRIVILEGES;
-EOSQL
